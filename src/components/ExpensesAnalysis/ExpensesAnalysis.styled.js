faca17453d826ad5066338dedbebfbffdbc7316c
Anton Nikonov <anick.on.off@gmail.com>
2026-04-16 11:48:45 +0700

    Удалено свойство min-height из компонента Swrapper

--- src/components/ExpensesAnalysis/ExpensesAnalysis.styled.js
+++ src/components/ExpensesAnalysis/ExpensesAnalysis.styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const Swrapper = styled.div`
     max-width: 100%;
     width: 100vw;
-    min-height: 100vh;
     overflow: hidden;
     background-color: #f4f5f6;
     position: relative;
